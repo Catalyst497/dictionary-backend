6a83a763fc32c4add8bf0e6f8965c26ce63ee7fa
Catalyst <danielduyilemi@gmail.com>
2024-01-04 10:47:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const User = require("./models/User.model");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const authRoutes = require('./auth/authRoutes')
+const userRoutes = require('./auth/userRoutes')
+require("dotenv").config();
+const JSON_SECRET = process.env.JSON_SECRET;
+
+require("./helpers/init_mongodb");
+require("./auth/auth_config");
+
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
+
+// Routes
+app.use(authRoutes)
+app.use(userRoutes)
+
+app.get("/", (req, res) => {
+  res.send("Hello World. Backend is connected.");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, (err) => (err ? console.log(err) : console.log("Plus Ultra")));
